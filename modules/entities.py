cdf59a09df55f5e1d6c1def7f472211aa1c85407
AndreFGard <andrefsagh@gmail.com>
2024-03-16 13:15:23 -0300

    [0.0.20] remover a flutuacao do personagem

--- modules/entities.py
+++ modules/entities.py
@@ -12,7 +12,7 @@ class PhysicsEntity:
         self.apply_gravity = True
         #esse offset tira o padding que as imagens das animacoes tem como margem
         # pra representar seus movimentos
-        self.anim_offset = (-3, -3)
+        self.anim_offset = (0, 0)
         self.movement_multiplier = 1
         
         #pra caso ele esteja virado pro outro lado
@@ -94,9 +94,8 @@ class PhysicsEntity:
         #o coordinate_system_scale permite que se renderize a entidade
         #num espaço de coordenadas onde tudo é maior do que o espaço onde existe a entidade
         surface.blit(pygame.transform.flip(self.animation.img(), self.flip, False),
-                     k_vector(coordinate_system_scale,
                         sum_vectors(subtract_vectors(self.pos, offset),
-                                    self.anim_offset)))
+                                    self.anim_offset))
 
         # pygame.draw.rect(surface, (0,0,150), pygame.Rect(self.pos[0] -offset[0], self.pos[1] -offset[1], self.size[0], self.size[1]) )
         # surface.blit(self.game.assets['player'], 
